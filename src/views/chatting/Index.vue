7605c7df33678486db7f5a804518e92c9cbe1505
007hccp <007jacqueline@gmail.com>
2024-12-25 15:12:00 +0900

    feat: 사운드 및 이모티콘 표시기능 추가

--- src/views/chatting/Index.vue
+++ src/views/chatting/Index.vue
@@ -7,8 +7,10 @@
         :key="index"
         :class="['message', message.sender]"
       >
-        <p v-html="message.text"/>
+        <!-- <div v-if="message.sender === ""></div> -->
+        <p v-html="message.text" />
       </div>
+      <!-- <div ref="emojiContainer" class="emoji-container"></div> -->
     </div>
 
     <!-- Input Area -->
@@ -25,34 +27,108 @@
 </template>
 
 <script>
-import { mapActions } from 'vuex';
+import { mapActions } from "vuex";
+import { gsap } from "gsap";
+
+const SENTIMENT_TYPE = {
+  NEUTRAL: "NEUTRAL",
+  POSITIVE: "POSITIVE",
+  NEGATIVE: "NEGATIVE",
+  MIXED: "MIXED"
+};
+
+const POSITIVE_EMOJI_LIST = [
+  "\u{1F604}",
+  "\u{1F602}",
+  // "\u{1F972}",
+  "\u{1F607}",
+  "\u{1F60D}",
+  "\u{1F61A}",
+  "\u{1F61C}",
+  "\u{1F60E}",
+  "\u{1F973}",
+  "\u{1F920}"
+];
+
+const NEGATIVE_EMOJI_LIST = [
+  "\u{1F612}",
+  "\u{1F614}",
+  "\u{1F62B}",
+  "\u{1F622}",
+  "\u{1F621}",
+  "\u{1F633}",
+  "\u{1F630}",
+  "\u{1F613}",
+  "\u{1F627}",
+  "\u{1F922}"
+];
+
+const NEUTRAL_EMOJI_LIST = [
+  "\u{1FAE1}",
+  "\u{1FAE0}",
+  "\u{1F610}",
+  "\u{1F62F}",
+  "\u{1F971}",
+  "\u{1F921}",
+  "\u{1F916}",
+  "\u{1F636}",
+  "\u{1F636}",
+  "\u{1F62A}"
+];
+
+const MIXED_EMOJI_LIST = [
+  "\u{1FAE1}",
+  "\u{1FAE1}",
+  "\u{1FAE1}",
+  "\u{1FAE1}",
+  "\u{1FAE1}",
+  "\u{1FAE1}",
+  "\u{1FAE1}",
+  "\u{1FAE1}",
+  "\u{1FAE1}",
+  "\u{1FAE1}"
+];
+
+const POSITIVE_SOUND_LIST = [
+  "/sounds/positive/1F60D_Positive_mixdown.mp3",
+  "/sounds/positive/1F60E_Positive_mixdown.mp3",
+  "/sounds/positive/1F61A_Positive_mixdown.mp3",
+  "/sounds/positive/1F61C_Positive_mixdown.mp3",
+  "/sounds/positive/1F602_Positive_mixdown.mp3",
+  "/sounds/positive/1F604_Positive_mixdown.mp3",
+  "/sounds/positive/1F607_Positive_mixdown.mp3",
+  "/sounds/positive/1F972_Positive_mixdown.mp3",
+]
 
 export default {
   data() {
     return {
-      inputMessage: '',
+      inputMessage: "",
       messages: [
-        { text: 'Hello! How are you feeling?', sender: 'bot' },
-        { text: 'Hi! I have a question.', sender: 'user' },
+        { text: "Hello! How are you feeling?", sender: "bot" },
+        { text: "Hi! I have aX question.", sender: "user" }
       ],
+      audio: null
     };
   },
   methods: {
-		...mapActions(['fetchData']),
+    ...mapActions(["fetchData"]),
     async sendMessage() {
-      if (this.inputMessage.trim() === '') return;
+      if (this.inputMessage.trim() === "") return;
 
       // Add user message
-      this.messages.push({ text: this.inputMessage, sender: 'user' });
-			
-			const text = this.inputMessage;
-      this.inputMessage = '';
+      this.messages.push({ text: this.inputMessage, sender: "user" });
+
+      const text = this.inputMessage;
+      this.inputMessage = "";
 
       // Simulate bot response
-			const response = await this.fetchData({ text })
-			console.dir(response);
-			this.messages.push({
-				text: `
+      const response = await this.fetchData({ text });
+      this.showEmojis(response.data.sentiment);
+      this.playAudio(response.data.sentiment);
+
+      this.messages.push({
+        text: `
 					sentiment: ${response.data.sentiment}<br/>
 					sentimentScores<br/>
 					-mixed: ${Number(response.data.sentimentScores.Mixed).toFixed(2)}<br/>
@@ -60,8 +136,8 @@ export default {
 					-neutral: ${Number(response.data.sentimentScores.Neutral).toFixed(2)}<br/>
 					-positive: ${Number(response.data.sentimentScores.Positive).toFixed(2)}
 				`,
-				sender: 'bot',
-			})
+        sender: "bot"
+      });
 
       this.scrollToBottom(); // Scroll after user message
     },
@@ -71,10 +147,64 @@ export default {
         chatWindow.scrollTop = chatWindow.scrollHeight;
       }
     },
+    showEmojis(sentiment) {
+      const emojiList = this.getEmojiList(sentiment);
+      const emojisToAdd = 10; // 한 번에 추가할 이모티콘 개수
+
+      for (let i = 0; i < emojisToAdd; i++) {
+        const emoji = document.createElement("span");
+        emoji.textContent =
+          emojiList[Math.floor(Math.random() * emojiList.length)];
+        emoji.style.position = "absolute";
+        emoji.style.left = `${Math.random() * 100}%`;
+        emoji.style.bottom = "0";
+
+        // 랜덤 크기 설정
+        const randomSize = Math.random() * 5 + 1; // 크기를 1rem ~ 3rem로 설정
+        emoji.style.fontSize = `${randomSize}rem`;
+        emoji.style.zIndex = "999";
+
+        this.$refs.chatWindow.appendChild(emoji);
+
+        // GSAP 애니메이션FF
+        gsap.to(emoji, {
+          // y: -400,
+          y: -window.innerHeight - 150,
+          opacity: 0,
+          duration: 3,
+          onComplete: () => emoji.remove(),
+          delay: Math.random() * 0.5 // 애니메이션 시작 지연 시간 (랜덤)
+        });
+      }
+    },
+    getEmojiList(sentiment) {
+      switch (sentiment) {
+        case SENTIMENT_TYPE.MIXED:
+          return MIXED_EMOJI_LIST;
+        case SENTIMENT_TYPE.NEGATIVE:
+          return NEGATIVE_EMOJI_LIST;
+        case SENTIMENT_TYPE.NEUTRAL:
+          return NEUTRAL_EMOJI_LIST;
+        case SENTIMENT_TYPE.POSITIVE:
+          return POSITIVE_EMOJI_LIST;
+        default:
+          return ["🎉", "❤️", "😂", "👍", "✨", "🥳"];
+      }
+    },
+    playAudio(/*sentiment*/) {
+      this.audio = new Audio(this.getSound());
+      this.audio.play();
+    },
+    getSound(/*sentiment*/) {
+      const soundList = POSITIVE_SOUND_LIST;
+      // TODO: switch
+
+      return soundList[Math.floor(Math.random() * soundList.length)];
+    }
   },
   updated() {
     this.scrollToBottom(); // Ensure scroll after DOM update
-  },
+  }
 };
 </script>
 
@@ -107,7 +237,7 @@ export default {
   border-radius: 10px;
   word-wrap: break-word;
   font-size: 14px;
-	text-align: left;
+  text-align: left;
 }
 
 .message.user {
@@ -148,5 +278,13 @@ export default {
       background-color: #0056b3;
     }
   }
+}
+
+.emoji-container {
+  position: relative;
+  width: 100%;
+  height: 400px;
+  overflow: hidden;
+  background-color: #f0f0f0;
 }
 </style>
